eb6675836329203e03753967edf9c5618ff53cd0
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-03-18 17:58:20 +0000

    add homebrew to install packages on osx

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+os_family="$(uname -s)"
+
+if [ $os_family = "Darwin" ]; then
+	ansible-galaxy collection install community.general
+fi
+
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook ./roles/main.yml --user $(whoami) --ask-pass --ask-become-pass
