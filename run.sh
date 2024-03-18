850ccfe6303e494669dfa17a683faf7ee5632ef7
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-03-18 19:41:29 +0000

    enable comminuty.general for all os families

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 
 os_family="$(uname -s)"
 
-if [ $os_family = "Darwin" ]; then
-	ansible-galaxy collection install community.general
-fi
-
 if [ $os_family = "Debian" ]; then
 	ansible-galaxy install -r requirements.debian.yml
 fi
 
+ansible-galaxy collection install community.general
+
 ansible-playbook ./roles/main.yml --user $(whoami) --ask-pass --ask-become-pass
+
